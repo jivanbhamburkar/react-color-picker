6cac1b0644fd6006be2d06ca60093927c799ad29
jivanbhamburkar <96575075+jivanbhamburkar@users.noreply.github.com>
2022-02-03 19:19:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React from "react";
+import "./App.css";
+import ColorBlock from "./components/colorBlock";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      color: "lightgray"
+    };
+  }
+  updateColor = (colorValue) => {
+    this.setState({
+      color: colorValue
+    });
+  };
+  render() {
+    return (
+      <>
+        <div className="color-container">
+          <ColorBlock color="red" onClick={this.updateColor} />
+          <ColorBlock color="green" onClick={this.updateColor} />
+          <ColorBlock color="orange" onClick={this.updateColor} />
+          <ColorBlock color="blue" onClick={this.updateColor} />
+          <ColorBlock color="cyan" onClick={this.updateColor} />
+          {/*
+
+            Code Smell
+
+           <div
+            className="color-block"
+            onClick={() => {
+              this.updateColor("red");
+            }}
+            style={{ backgroundColor: "red" }}
+          ></div>
+          <div
+            className="color-block"
+            onClick={() => {
+              this.updateColor("green");
+            }}
+            style={{ backgroundColor: "green" }}
+          ></div>
+          <div
+            className="color-block"
+            onClick={() => {
+              this.updateColor("orange");
+            }}
+            style={{ backgroundColor: "orange" }}
+          ></div>
+          <div
+            className="color-block"
+            onClick={() => {
+              this.updateColor("blue");
+            }}
+            style={{ backgroundColor: "blue" }}
+          ></div>
+          <div
+            className="color-block"
+            onClick={() => {
+              this.updateColor("cyan");
+            }}
+            style={{ backgroundColor: "cyan" }}
+          ></div> */}
+        </div>
+        <div
+          className="target-div"
+          style={{
+            backgroundColor: this.state.color
+          }}
+        ></div>
+      </>
+    );
+  }
+}
+
+export default App;
